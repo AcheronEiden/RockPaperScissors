4704ebc9c0cad62ec426c71a4ac01c8ebceca46e
AcheronEiden <alhassan0000@yahoo.com>
2022-07-01 17:49:07 +0200

    QuickUpdate
    What's completed:
    - Made it so that the player can't type anything other than rock, paper or scissor
    - Made it so that the game can work whether the player writs in lower-case, upper-case or a combination of case types
    - Made it so that the program asks if the player wants to play again or not and depending on the answer, restart the program from line 1 or runs line 51
    
    The next update:
    - Maybe make it so that the player can answer outside of yes/no to the play again question?

--- TheGame.py
+++ TheGame.py
@@ -1,36 +1,51 @@
 import random
+while True:
+    # The following code lines creates the computers choice
 
-computer_choices = ["rock", "paper", "scissors"]
-computer = random.choice(computer_choices)
-user = input("What's your choice?:")
+    computer_choices = ["rock", "paper", "scissors"]
+    computer = random.choice(computer_choices)
 
-if computer == user:
-    print("Computer: ", computer)
-    print("You: ", user)
-    print("It's a draw :|")
-elif computer == "rock":
-    if user == "paper":
+    # The following code lines invites the player to choose their hand. The while loop is
+    # there so that the player can't choose alternatives outside of rock, paper or scissors
+
+    user = None
+    while user not in computer_choices:
+        user = input("What's your choice?: ").lower() # so it doesn't matter if the player writes in lower-case, upper-case or a combination
+
+    if computer == user:
         print("Computer: ", computer)
-        print("You: ", user)        
-        print("You won :)")
-    elif user == "scissors":
-        print("Computer: ", computer)
-        print("You: ", user)        
-        print("You lose :(")
-elif computer == "paper":
-    if user == "rock":
-        print("Computer: ", computer)
-        print("You lose :(")
-    elif user == "scissors":
-        print("Computer: ", computer)
-        print("You: ", user)        
-        print("You win :)")
-elif computer == "scissors":
-    if user == "paper":
-        print("Computer: ", computer)
-        print("You: ", user)        
-        print("You lose :(")
-    elif user == "rock":
-        print("Computer: ", computer)
-        print("You: ", user)        
-        print("You win :)")
+        print("You: ", user)
+        print("It's a draw :|")
+    elif computer == "rock":
+        if user == "paper":
+            print("Computer: ", computer)
+            print("You: ", user)        
+            print("You won :)")
+        elif user == "scissors":
+            print("Computer: ", computer)
+            print("You: ", user)        
+            print("You lose :(")
+    elif computer == "paper":
+        if user == "rock":
+            print("Computer: ", computer)
+            print("You lose :(")
+        elif user == "scissors":
+            print("Computer: ", computer)
+            print("You: ", user)        
+            print("You win :)")
+    elif computer == "scissors":
+        if user == "paper":
+            print("Computer: ", computer)
+            print("You: ", user)        
+            print("You lose :(")
+        elif user == "rock":
+            print("Computer: ", computer)
+            print("You: ", user)        
+            print("You win :)")
+            
+    # The following code lines lets the program ask the player if they want to play again
+    try_again = input("Want to play again?: ").lower()
+    if try_again != "yes":
+        break
+
+print("Then see you later!")
